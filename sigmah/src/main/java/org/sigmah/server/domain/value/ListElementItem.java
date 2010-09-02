6c4f970d27a1700e9fb4c210d57e5abc59fd7fbb
raphael.calabro@gmail.com <raphael.calabro@gmail.com@46f870f5-83fa-7f44-208d-45d3d0f2e120>
2010-09-02 08:13:03 +0000

    Sigmah can now save phases changes

---
+++ sigmah/src/main/java/org/sigmah/server/domain/value/ListElementItem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sigmah.server.domain.value;
+
+/**
+ *
+ * @author rca
+ */
+public interface ListElementItem {
+    void setIdList(Long id);
+}
---
+++ sigmah/src/main/java/org/sigmah/shared/dto/value/ListElementItemDTO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sigmah.shared.dto.value;
+
+/**
+ *
+ * @author Raphaël Calabro (rcalabro@ideia.fr)
+ */
+public interface ListElementItemDTO {
+    int getIdList();
+    int getIndex();
+    String getEntityName();
+}
