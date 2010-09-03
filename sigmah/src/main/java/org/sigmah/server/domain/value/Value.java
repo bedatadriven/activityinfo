ea6889728e0b9ddc026fc4464daa399df9a6d657
denis.colliot <denis.colliot@46f870f5-83fa-7f44-208d-45d3d0f2e120>
2010-09-03 14:56:59 +0000

    Modification of a "OneToOne" annotation into a "ManyToOne" annotation.

--- sigmah/src/main/java/org/sigmah/server/domain/value/Value.java
+++ sigmah/src/main/java/org/sigmah/server/domain/value/Value.java
@@ -10,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -29,8 +28,7 @@ import org.sigmah.server.domain.element.TextAreaElement;
  */
 @Entity
 @Table(name = "value", uniqueConstraints = { @UniqueConstraint(columnNames = {
-																				"id_flexible_element",
-																				"id_project" }) })
+		"id_flexible_element", "id_project" }) })
 public class Value implements Serializable {
 
 	private static final long serialVersionUID = -2578586689736955636L;
@@ -58,7 +56,7 @@ public class Value implements Serializable {
 		this.element = element;
 	}
 
-	@OneToOne(optional = false)
+	@ManyToOne(optional = false)
 	@JoinColumn(name = "id_flexible_element", nullable = false)
 	public FlexibleElement getElement() {
 		return element;
@@ -112,141 +110,10 @@ public class Value implements Serializable {
 		return lastModificationAction;
 	}
 
-//	@Override
-//	public String toString() {
-//		final StringBuilder sb = new StringBuilder();
-//		sb.append(this.getClass().getSimpleName());
-//		sb.append(" (");
-//		sb.append("id -> ");
-//		sb.append(id);
-//		sb.append(", ");
-//		sb.append("element -> ");
-//		sb.append(element != null ? element.getId() : "null");
-//		sb.append(", ");
-//		sb.append("phase -> ");
-//		sb.append(parentProject != null ? parentProject.getId() : "null");
-//		sb.append(", ");
-//		sb.append("value -> ");
-//		sb.append(value);
-//		sb.append(")");
-//
-//		// Displays the value if it is a complex type.
-//		if (element instanceof QuestionElement) {
-//
-//			final QuestionChoiceElement choice = DummyService.getQuestionChoiceElement(Long.valueOf(value));
-//
-//			sb.append(" ==> ");
-//			sb.append(choice.getLabel());
-//		} else if (element instanceof FilesListElement) {
-//
-//			final List<FilesListValue> values = DummyService.getFilesListValue(Long.valueOf(value));
-//
-//			sb.append(" ==> ");
-//			sb.append(FilesListValue.class.getSimpleName());
-//			sb.append(" (");
-//			sb.append(values != null ? values.size() : 0);
-//			sb.append(") (");
-//
-//			if (values != null) {
-//				for (final FilesListValue value : values) {
-//					final File file = value.getFile();
-//					sb.append(File.class.getSimpleName());
-//					sb.append(" (");
-//					sb.append("name -> ");
-//					sb.append(file.getName());
-//					sb.append(", ");
-//					sb.append("versions -> ");
-//					sb.append(file.getVersions().size());
-//					sb.append("), ");
-//				}
-//			}
-//			sb.append(")");
-//		} else if (element instanceof TripletsListElement) {
-//
-//			final List<TripletValue> values = DummyService.getTripletsListValue(Long.valueOf(value));
-//
-//			sb.append(" ==> ");
-//			sb.append(TripletValue.class.getSimpleName());
-//			sb.append(" (");
-//			sb.append(values != null ? values.size() : 0);
-//			sb.append(") (");
-//
-//			if (values != null) {
-//				for (final TripletValue triplet : values) {
-//					sb.append(" (");
-//					sb.append("code -> ");
-//					sb.append(triplet.getCode());
-//					sb.append(", ");
-//					sb.append("name -> ");
-//					sb.append(triplet.getName());
-//					sb.append(", ");
-//					sb.append("period -> ");
-//					sb.append(triplet.getPeriod());
-//					sb.append("), ");
-//				}
-//			}
-//			sb.append(")");
-//		} else if (element instanceof IndicatorsListElement) {
-//
-//			final List<IndicatorsListValue> values = DummyService.getIndicatorsListValue(Long.valueOf(value));
-//
-//			sb.append(" ==> ");
-//			sb.append(IndicatorsListValue.class.getSimpleName());
-//			sb.append(" (");
-//			sb.append(values != null ? values.size() : 0);
-//			sb.append(") (");
-//
-//			if (values != null) {
-//				for (final IndicatorsListValue value : values) {
-//					final Indicator ind = value.getIndicator();
-//					sb.append(Indicator.class.getSimpleName());
-//					sb.append(" (");
-//					sb.append("value -> ");
-//					sb.append(ind.getValue());
-//					sb.append(", ");
-//					sb.append("quality -> ");
-//					sb.append(ind.getQualityCriterion());
-//					sb.append("), ");
-//				}
-//			}
-//			sb.append(")");
-//		} else if (element instanceof BudgetDistributionElement) {
-//
-//			final BudgetPartsListValue dist = DummyService.getBudgetPartsListValue(Long.valueOf(value));
-//
-//			sb.append(" ==> ");
-//			sb.append(BudgetPartsListValue.class.getSimpleName());
-//			sb.append(" (");
-//			sb.append("budget id -> ");
-//			sb.append(dist.getBudget().getId());
-//			sb.append(", ");
-//			sb.append("budget amount -> ");
-//			sb.append(dist.getBudget().getAmount());
-//			sb.append(") (");
-//			sb.append(dist != null ? dist.getParts().size() : 0);
-//			sb.append(") (");
-//
-//			if (dist != null) {
-//				for (final BudgetPart part : dist.getParts()) {
-//					sb.append(BudgetPart.class.getSimpleName());
-//					sb.append(" (");
-//					sb.append("label -> ");
-//					sb.append(part.getLabel());
-//					sb.append(", ");
-//					sb.append("amount -> ");
-//					sb.append(part.getAmount());
-//					sb.append("), ");
-//				}
-//			}
-//			sb.append(")");
-//		}
-//
-//		return sb.toString();
-//	}
-
 	/**
-	 * Returns if the value can be consider as <code>valid</code> (correctly filled) according to its type.
-	 *
+	 * Returns if the value can be consider as <code>valid</code> (correctly
+	 * filled) according to its type.
+	 * 
 	 * @return if the value is valid.
 	 */
 	@Transient
