e9491788a38fa439df8c388dbb6a1b0614faf282
tom.miette <tom.miette@46f870f5-83fa-7f44-208d-45d3d0f2e120>
2010-09-06 13:12:24 +0000

    Adds some useful logs to show openings and closings of EM transactions managed by the TransactionalInterceptor.

--- sigmah/src/main/java/org/sigmah/server/dao/hibernate/TransactionalInterceptor.java
+++ sigmah/src/main/java/org/sigmah/server/dao/hibernate/TransactionalInterceptor.java
@@ -5,13 +5,16 @@
 
 package org.sigmah.server.dao.hibernate;
 
-import com.google.inject.Inject;
-import com.google.inject.Injector;
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
 
 /**
  * MethodInterceptor that implements declarative transaction management.
@@ -21,6 +24,11 @@ import javax.persistence.EntityTransaction;
  */
 public class TransactionalInterceptor implements MethodInterceptor {
 
+    /**
+     * Logger.
+     */
+    private static final Log log = LogFactory.getLog(TransactionalInterceptor.class);
+    
     private Injector injector;
 
     @Inject
@@ -35,15 +43,30 @@ public class TransactionalInterceptor implements MethodInterceptor {
 
         //allow joining of transactions if there is an enclosing @Transactional method
         if (tx.isActive()) {
+            
+            if (log.isDebugEnabled()) {
+                log.debug("[invoke] Transaction already active, just proceed the method.");
+            }
+            
             return methodInvocation.proceed();
         }
 
         tx.begin();
+        
+        if (log.isDebugEnabled()) {
+            log.debug("[invoke] Begins an entity transaction.");
+        }
+        
         Object result = attemptInvocation(methodInvocation, tx);
 
         // everything was normal so commit the txn (do not move into try block as it interferes
         // with the advised method's throwing semantics)
         tx.commit();
+        
+        if (log.isDebugEnabled()) {
+            log.debug("[invoke] Commits the transaction.");
+        }
+        
         return result;
     }
 
