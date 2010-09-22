c914879892aab80cfa5913d0b27b74180a52ffe8
tom.miette <tom.miette@46f870f5-83fa-7f44-208d-45d3d0f2e120>
2010-09-22 11:49:12 +0000

    Modified the values retrieval : used two queries (one query to get the value and eventually a second one to sub-select the real-values) instead of only one with a join.

--- sigmah/src/main/java/org/sigmah/server/endpoint/gwtrpc/handler/GetValueHandler.java
+++ sigmah/src/main/java/org/sigmah/server/endpoint/gwtrpc/handler/GetValueHandler.java
@@ -22,11 +22,6 @@ import org.sigmah.shared.command.handler.CommandHandler;
 import org.sigmah.shared.command.result.CommandResult;
 import org.sigmah.shared.command.result.ValueResult;
 import org.sigmah.shared.domain.User;
-import org.sigmah.shared.dto.element.BudgetDistributionElementDTO;
-import org.sigmah.shared.dto.element.FilesListElementDTO;
-import org.sigmah.shared.dto.element.IndicatorsListElementDTO;
-import org.sigmah.shared.dto.element.MessageElementDTO;
-import org.sigmah.shared.dto.element.TripletsListElementDTO;
 import org.sigmah.shared.dto.value.BudgetPartsListValueDTO;
 import org.sigmah.shared.dto.value.FilesListValueDTO;
 import org.sigmah.shared.dto.value.IndicatorsListValueDTO;
@@ -41,8 +36,8 @@ import com.google.inject.Inject;
  * @author Denis Colliot (dcolliot@ideia.fr)
  */
 public class GetValueHandler implements CommandHandler<GetValue> {
-	
-	private final static Log LOG = LogFactory.getLog(GetValueHandler.class);
+
+    private final static Log LOG = LogFactory.getLog(GetValueHandler.class);
 
     private final EntityManager em;
     private final Mapper mapper;
@@ -56,120 +51,142 @@ public class GetValueHandler implements CommandHandler<GetValue> {
     /**
      * Gets a flexible element value from the database.
      * 
-     * @param cmd {@link GetValue} command containing the flexible element class, its id, and the project id
-     * @param user user connected
+     * @param cmd
+     *            {@link GetValue} command containing the flexible element
+     *            class, its id, and the project id
+     * @param user
+     *            user connected
      * 
-     * @return a {@link ValueResult} object containing the value of the flexible element or containing 
-     * {@code null} if there is no value defined for this element.
+     * @return a {@link ValueResult} object containing the value of the flexible
+     *         element or containing {@code null} if there is no value defined
+     *         for this element.
      */
     @Override
     public CommandResult execute(GetValue cmd, User user) throws CommandException {
-    	if (LOG.isDebugEnabled()) {
-    		LOG.debug("[execute] Getting value object from the database.");
-    	}
-    	
-    	String queryString = null;
-    	String elementClassName = cmd.getElementEntityName();
-    	Class<? extends Serializable> clazz = null;
-    	ValueResult valueResult = new ValueResult();
-    	boolean isList = false;
-    	
-    	if (LOG.isDebugEnabled()) {
-    		LOG.debug("[execute] GetValue command = " + cmd.toString() + ".");
-    	}
-    	
-		if (elementClassName.equals("element.TripletsListElement")) {
-			if (LOG.isDebugEnabled()) {
-	    		LOG.debug("[execute] Case TripletsListElementDTO.");
-	    	}
-			queryString = "select tv from TripletValue tv, Value v where tv.idList = v.value and v.parentProject.id = :projectId and v.element.id = :elementId";
-			clazz = TripletValueDTO.class;
-			isList = true;
-		}
-		else if (elementClassName.equals("element.IndicatorsListElement")) {
-			if (LOG.isDebugEnabled()) {
-	    		LOG.debug("[execute] Case IndicatorsListElementDTO.");
-	    	}
-			queryString = "select ilv from IndicatorsListValue ilv, Value v where ilv.id.idList = v.value and v.parentProject.id = :projectId and v.element.id = :elementId";
-			clazz = IndicatorsListValueDTO.class;
-			isList = true;
-		}
-		else if (elementClassName.equals("element.BudgetDistributionElement")) {
-			if (LOG.isDebugEnabled()) {
-	    		LOG.debug("[execute] Case BudgetDistributionElementDTO.");
-	    	}
-			queryString = "select bplv from BudgetPartsListValue bplv, Value v where bplv.id = v.value and v.parentProject.id = :projectId and v.element.id = :elementId";
-			clazz = BudgetPartsListValueDTO.class;
-			isList = true;
-		}
-		else if (elementClassName.equals("element.FilesListElement")) {
-			if (LOG.isDebugEnabled()) {
-	    		LOG.debug("[execute] Case FilesListElementDTO.");
-	    	}
-			queryString = "select flv from FilesListValue flv, Value v where flv.id.idList = v.value and v.parentProject.id = :projectId and v.element.id = :elementId";
-			clazz = FilesListValueDTO.class;
-			isList = true;
-		}
-		else if (!(elementClassName.equals("element.MessageElement"))) {
-			if (LOG.isDebugEnabled()) {
-	    		LOG.debug("[execute] Case others (but MessageElementDTO).");
-	    	}
-			queryString = "select v.value from Value v where v.parentProject.id = :projectId and v.element.id = :elementId";
-			clazz = String.class;
-		}
-		
-		if (queryString != null) {
-			Query query = em.createQuery(queryString);
-			query.setParameter("projectId", cmd.getProjectId());
-			query.setParameter("elementId", cmd.getElementId());
-	    	
-			// Multiple results case
-			if (isList) {
-				if (LOG.isDebugEnabled()) {
-		    		LOG.debug("[execute] Multiple values for the element #" + cmd.getElementId() + ".");
-		    	}
-				@SuppressWarnings("unchecked")
-				List<Object> objectsList = query.getResultList();
-				
-				List<Serializable> serializablesList = new ArrayList<Serializable>();
-				for (Object o : objectsList) {
-					serializablesList.add(mapper.map(o, clazz));
-				}
-				
-				valueResult.setValuesObject(serializablesList);
-			}
-			// Single result case
-			else {
-				if (LOG.isDebugEnabled()) {
-		    		LOG.debug("[execute] Single value for the element #" + cmd.getElementId() + ".");
-		    	}
-				try {
-					Object object = query.getSingleResult();
-					if (object != null) {
-						if (clazz.equals(String.class)) {
-							valueResult.setValueObject(String.valueOf(object));
-						}
-						else {
-							valueResult.setValueObject(mapper.map(object, clazz));
-						}
-					}
-				}
-				catch (NoResultException nre) {
-					if (LOG.isDebugEnabled()) {
-			    		LOG.debug("[execute] No value for this flexible element #" + cmd.getElementId() + ".");
-			    	}
-				}
-			}
-			
-			valueResult.setListResult(isList);
-		}
-		
-		if (LOG.isDebugEnabled()) {
-    		LOG.debug("[execute] Returned value = " + valueResult.toString() + ".");
-    	}
-		
-		return valueResult;
-		
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("[execute] Getting value object from the database.");
+            LOG.debug("[execute] GetValue command = " + cmd.toString() + ".");
+        }
+
+        final ValueResult valueResult = new ValueResult();
+
+        // Creates the query to get the value for the flexible element (as
+        // string) in the Value table.
+        final Query valueQuery = em
+                .createQuery("SELECT v.value FROM Value v WHERE v.parentProject.id = :projectId AND v.element.id = :elementId");
+        valueQuery.setParameter("projectId", cmd.getProjectId());
+        valueQuery.setParameter("elementId", cmd.getElementId());
+
+        // Executes the query and tests if a value exists for this flexible
+        // element.
+        boolean isValueExisting = true;
+        String valueAsString = null;
+        try {
+            valueAsString = (String) valueQuery.getSingleResult();
+            if (valueAsString == null) {
+                isValueExisting = false;
+            }
+        } catch (NoResultException e) {
+            isValueExisting = false;
+        } catch (ClassCastException e) {
+            isValueExisting = false;
+        }
+
+        // No value exists for the flexible element.
+        if (!isValueExisting) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("[execute] No value for this flexible element #" + cmd.getElementId() + ".");
+            }
+            return valueResult;
+        }
+
+        String queryString = null;
+        String elementClassName = cmd.getElementEntityName();
+        Class<? extends Serializable> clazz = null;
+        boolean isList = false;
+
+        // Creates the sub-select query to get the real value.
+        if (elementClassName.equals("element.TripletsListElement")) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("[execute] Case TripletsListElementDTO.");
+            }
+            queryString = "SELECT tv FROM TripletValue tv WHERE tv.idList = :value";
+            clazz = TripletValueDTO.class;
+            isList = true;
+        } else if (elementClassName.equals("element.IndicatorsListElement")) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("[execute] Case IndicatorsListElementDTO.");
+            }
+            queryString = "SELECT ilv FROM IndicatorsListValue ilv WHERE ilv.id.idList = :value";
+            clazz = IndicatorsListValueDTO.class;
+            isList = true;
+        } else if (elementClassName.equals("element.BudgetDistributionElement")) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("[execute] Case BudgetDistributionElementDTO.");
+            }
+            queryString = "SELECT bplv FROM BudgetPartsListValue bplv WHERE bplv.id = :value";
+            clazz = BudgetPartsListValueDTO.class;
+            isList = true;
+        } else if (elementClassName.equals("element.FilesListElement")) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("[execute] Case FilesListElementDTO.");
+            }
+            queryString = "SELECT flv FROM FilesListValue flv WHERE flv.id.idList = :value";
+            clazz = FilesListValueDTO.class;
+            isList = true;
+        } else if (!(elementClassName.equals("element.MessageElement"))) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("[execute] Case others (but MessageElementDTO).");
+            }
+            queryString = "";
+            clazz = String.class;
+            isList = false;
+        }
+
+        if (queryString != null) {
+            // Multiple results case
+            if (isList) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("[execute] Multiple values for the element #" + cmd.getElementId() + ".");
+                }
+
+                final Query query = em.createQuery(queryString);
+                // The value is casted as a long to perform the sub-select
+                // query.
+                query.setParameter("value", Long.valueOf(valueAsString));
+
+                @SuppressWarnings("unchecked")
+                final List<Object> objectsList = query.getResultList();
+
+                final List<Serializable> serializablesList = new ArrayList<Serializable>();
+                for (Object o : objectsList) {
+                    serializablesList.add(mapper.map(o, clazz));
+                }
+
+                valueResult.setValuesObject(serializablesList);
+            }
+            // Single result case
+            else {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("[execute] Single value for the element #" + cmd.getElementId() + ".");
+                }
+
+                if (clazz.equals(String.class)) {
+                    valueResult.setValueObject(valueAsString);
+                } else {
+                    valueResult.setValueObject(mapper.map(valueAsString, clazz));
+                }
+            }
+
+            valueResult.setListResult(isList);
+        }
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("[execute] Returned value = " + valueResult.toString() + ".");
+        }
+
+        return valueResult;
+
     }
-    
 }
