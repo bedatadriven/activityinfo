8cb157603474b354e22da71f4ee664eaeb4fed1c
tom.miette <tom.miette@46f870f5-83fa-7f44-208d-45d3d0f2e120>
2010-10-20 15:17:38 +0000

    Changed default user email.

--- sigmah/src/main/java/org/sigmah/public/Sigmah.params.js
+++ sigmah/src/main/java/org/sigmah/public/Sigmah.params.js
@@ -5,7 +5,7 @@
 var SigmahParams = {
     connectedUserId: 4,
     connectedUserAuthToken: "6b18af1b85653a4fe44f357aaa3f1071",
-    connectedUserEmail: "osarrat@sigmah.org",
+    connectedUserEmail: "pmartin@sigmah.org",
     showActivityInfoMenus: true
 };
 
