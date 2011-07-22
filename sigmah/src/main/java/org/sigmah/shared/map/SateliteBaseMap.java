8466572065984cf4e402755e80427715c869ad7f
ruud <ruud@bedatadriven.com>
2011-07-22 19:13:09 +0200

    Fixed map types without a name

--- sigmah/src/main/java/org/sigmah/shared/map/SateliteBaseMap.java
+++ sigmah/src/main/java/org/sigmah/shared/map/SateliteBaseMap.java
@@ -2,4 +2,29 @@ package org.sigmah.shared.map;
 
 public class SateliteBaseMap extends BaseMap {
 
+	// TODO: Add i18n when serverside i18n is implemented
+	@Override
+	public String getName() {
+		return "Google satelite";
+	}
+
+	@Override
+	public int getMinZoom() {
+		return 1;
+	}
+
+	@Override
+	public int getMaxZoom() {
+		return 21;
+	}
+
+	@Override
+	public String getCopyright() {
+		return "Google inc.";
+	}
+
+	@Override
+	public void setCopyright(String copyright) {
+	}
+
 }
--- sigmah/src/main/java/org/sigmah/shared/map/StreetBaseMap.java
+++ sigmah/src/main/java/org/sigmah/shared/map/StreetBaseMap.java
@@ -2,4 +2,20 @@ package org.sigmah.shared.map;
 
 public class StreetBaseMap extends BaseMap {
 
+	// TODO: Add i18n when serverside i18n is implemented
+	@Override
+	public String getName() {
+		return "Goolge maps";
+	}
+	
+	@Override
+	public int getMinZoom() {
+		return 1;
+	}
+
+	@Override
+	public int getMaxZoom() {
+		return 21;
+	}
+
 }
