9457de37add4eaba68b0a8e39ddcfc0e3439ba51
ruud <ruud@bedatadriven.com>
2011-07-22 19:03:25 +0200

    Added forgotten file of eGit

---
+++ sigmah/src/main/java/org/sigmah/server/util/ColorUtil.java
@@ -0,0 +1,23 @@
+package org.sigmah.server.util;
+
+import java.awt.Color;
+
+public class ColorUtil {
+	public static Color colorFromString(String color) {
+		if (color.startsWith("#")) {
+			color = color.substring(1);
+		}
+
+		Color result = new Color(0, 255, 0);
+		try {
+			result = new Color(Integer.parseInt(color));
+		} catch (NumberFormatException e) {
+			result = Color.decode("0x" + color);
+		}
+		return result;
+	}
+
+	public static int toInteger(String color) {
+		return colorFromString(color).getRGB();
+	}
+}
