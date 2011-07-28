623f6c6ba88a39d93ab2f910cfa76066aee5ad15
ruud <ruud@bedatadriven.com>
2011-07-28 11:38:08 +0200

    Fixed missing Project files

--- sigmah/src/main/java/org/sigmah/shared/domain/Project2.java
+++ sigmah/src/main/java/org/sigmah/shared/domain/Project2.java
@@ -1,21 +1,74 @@
 package org.sigmah.shared.domain;
 
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Project2 {
+	private int id;
+	private String name;
+	private int siteId;
+	private String description;
+	private Date dateDeleted;
+	
+	public Project2() {
+		super();
+	}
 
-    private int id;
+	public Project2(int id, String name, int siteId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.siteId = siteId;
+	}
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "ProjectId", unique = true, nullable = false)
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Column(nullable=false, length=30)
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getSiteId() {
+		return siteId;
+	}
+	
+	public void setSiteId(int siteId) {
+		this.siteId = siteId;
+	}
 
-    @Id
-    public int getId() {
-        return id;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	@Column(length=250, nullable=true)
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDateDeleted(Date dateDeleted) {
+		this.dateDeleted = dateDeleted;
+	}
+
+	public Date getDateDeleted() {
+		return dateDeleted;
+	}
 }
--- sigmah/src/main/java/org/sigmah/shared/dto/Project2DTO.java
+++ sigmah/src/main/java/org/sigmah/shared/dto/Project2DTO.java
@@ -2,5 +2,50 @@ package org.sigmah.shared.dto;
 
 import com.extjs.gxt.ui.client.data.BaseModelData;
 
-public class Project2DTO extends BaseModelData{
+public class Project2DTO extends BaseModelData implements DTO {
+	private String description;
+	public Project2DTO() {
+		super();
+	}
+
+//	public Project2DTO(int id, String name, int siteId) {
+	public Project2DTO(int id, String name) {
+		super();
+		
+		setId(id);
+		setName(name);
+//		setSiteId(siteId); 
+	} 
+
+	public int getId() {
+		return (Integer)get("id");
+	}
+	
+	public void setId(int id) {
+		set("id", id); 
+	}
+	
+	public String getName() {
+		return (String)get("name");
+	}
+	
+	public void setName(String name) {
+		set("name", name);
+	}
+	
+//	public int getSiteId() {
+//		return (Integer)get("siteId");
+//	}
+//	
+//	public void setSiteId(int siteId) {
+//		set("siteId", siteId);
+//	}
+
+	public void setDescription(String description) {
+		set("description", description);
+	}
+
+	public String getDescription() {
+		return (String)get("description");
+	}
 }
