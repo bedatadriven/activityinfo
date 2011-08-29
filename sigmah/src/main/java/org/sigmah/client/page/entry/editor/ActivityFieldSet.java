81e7a79b9c6d5a0000fe70ed52ecc4e71f063c6f
ruud <ruud@bedatadriven.com>
2011-08-29 14:55:58 +0200

    If database has no projects, user is presented with label telling him at Add/Edit Site Closes #66

--- sigmah/src/main/java/org/sigmah/client/page/entry/editor/ActivityFieldSet.java
+++ sigmah/src/main/java/org/sigmah/client/page/entry/editor/ActivityFieldSet.java
@@ -15,6 +15,7 @@ import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.widget.form.ComboBox;
 import com.extjs.gxt.ui.client.widget.form.DateField;
 import com.extjs.gxt.ui.client.widget.form.Field;
+import com.extjs.gxt.ui.client.widget.form.LabelField;
 import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.form.Validator;
 
@@ -86,17 +87,24 @@ public class ActivityFieldSet extends AbstractFieldSet {
 
 		}
 		
-		ComboBox<ProjectDTO> comboboxProjects = new ComboBox<ProjectDTO>();
-		
-		comboboxProjects.setName("project");
-		comboboxProjects.setDisplayField("name");
-		comboboxProjects.setEditable(false);
-		comboboxProjects.setStore(projectStore);
-		comboboxProjects.setTriggerAction(ComboBox.TriggerAction.ALL);
-		comboboxProjects.setFieldLabel(I18N.CONSTANTS.project());
-		comboboxProjects.setForceSelection(true);
-		comboboxProjects.setAllowBlank(false);
-		add(comboboxProjects);
+		if (activity.getDatabase().getProjects().size() > 0) {
+			ComboBox<ProjectDTO> comboboxProjects = new ComboBox<ProjectDTO>();
+			
+			comboboxProjects.setName("project");
+			comboboxProjects.setDisplayField("name");
+			comboboxProjects.setEditable(false);
+			comboboxProjects.setStore(projectStore);
+			comboboxProjects.setTriggerAction(ComboBox.TriggerAction.ALL);
+			comboboxProjects.setFieldLabel(I18N.CONSTANTS.project());
+			comboboxProjects.setForceSelection(true);
+			comboboxProjects.setAllowBlank(false);
+			add(comboboxProjects);
+		} else {
+			LabelField labelNoProjects = new LabelField(
+					I18N.MESSAGES.noProjectsDefinedForDatabase(activity.getDatabase().getName()));
+			labelNoProjects.setFieldLabel(I18N.CONSTANTS.projects());
+			add(labelNoProjects);
+		}
 
 //		if(activity.getLocationType().getBoundAdminLevelId() == null) {
 //			ComboBox<SiteDTO> assessmentCombo = new AssessmentCombo(activity.getDatabase().getCountry());
