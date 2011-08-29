90457b0333362b17d59c5814334b3163b43e1dbe
ruud <ruud@bedatadriven.com>
2011-08-29 13:00:01 +0200

    Fixed bugs related to SearchTest, use new AsyncCommandTest

--- sigmah/src/main/java/org/sigmah/shared/command/handler/SearchHandler.java
+++ sigmah/src/main/java/org/sigmah/shared/command/handler/SearchHandler.java
@@ -1,12 +1,10 @@
-package org.sigmah.server.endpoint.gwtrpc.handler;
+package org.sigmah.shared.command.handler;
 
 import org.sigmah.server.dao.SiteDAO;
+import org.sigmah.server.endpoint.gwtrpc.handler.GenerateElementHandler;
 import org.sigmah.shared.command.GenerateElement;
 import org.sigmah.shared.command.GetSites;
 import org.sigmah.shared.command.Search;
-import org.sigmah.shared.command.handler.CommandHandlerAsync;
-import org.sigmah.shared.command.handler.ExecutionContext;
-import org.sigmah.shared.command.handler.GetSitesHandler;
 import org.sigmah.shared.command.result.SearchResult;
 import org.sigmah.shared.command.result.SiteResult;
 import org.sigmah.shared.dao.Filter;
@@ -18,46 +16,65 @@ import org.sigmah.shared.report.model.PivotTableReportElement;
 import org.sigmah.shared.search.AllSearcher;
 import org.sigmah.shared.search.SearchException;
 
-import com.bedatadriven.rebar.sql.client.SqlDatabase;
+import com.bedatadriven.rebar.sql.client.SqlTransaction;
 import com.extjs.gxt.ui.client.Style.SortDir;
 import com.extjs.gxt.ui.client.data.SortInfo;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 
 public class SearchHandler implements CommandHandlerAsync<Search, SearchResult> {
-	private SqlDatabase db;
 	private GenerateElementHandler getPivotData;
 
     @Inject
-    public SearchHandler(SqlDatabase db, SiteDAO siteDAO, GenerateElementHandler genElHandler) {
-		this.db=db;
+    public SearchHandler(SiteDAO siteDAO, GenerateElementHandler genElHandler) {
 		this.getPivotData = genElHandler;
     }
 
 	@Override
-	public void execute(final Search command, ExecutionContext context,
+	public void execute(final Search command, final ExecutionContext context,
 			final AsyncCallback<SearchResult> callback) {
 		
 		final PivotTableReportElement pivotTable = createSearchPivotTableElement();
 		
-		createFilterFromQuery(command.getSearchQuery(), pivotTable, callback);
-		//pivotTable.getFilter().setOr(true);
-		PivotContent content = null;
-		final SearchResult result = new SearchResult();
+		createFilterFromQuery(command.getSearchQuery(), pivotTable, context.getTransaction(), new AsyncCallback<Filter>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				callback.onFailure(caught);
+			}
 
-		// Only request search results when restriciton are present, no need to query when no restrictions
-		if (pivotTable.getFilter().getRestrictedDimensions().size() > 0) {
-			content = getPivotDataUsingFilter(context, callback, pivotTable, content);
-			getRecentSitesUsingFilter(context, callback, pivotTable, result); 
-		}
-	
-		if (content == null) {
-			callback.onFailure(new SearchException("PivotContent is null. Probably an uncaught database or searcher error "));
-		} else {
-			result.setPivotTabelData(content);
-		}
-		
-		callback.onSuccess(result);
+			@Override
+			public void onSuccess(Filter resultFilter) {
+				//pivotTable.getFilter().setOr(true);
+				PivotContent content = null;
+				final SearchResult searchResult = new SearchResult();
+
+				// Only request search results when restriciton are present, no need to query when no restrictions
+				if (resultFilter.getRestrictedDimensions().size() > 0) {
+					
+					pivotTable.setFilter(resultFilter);
+					content = getPivotDataUsingFilter(context, callback, pivotTable, content);
+					if (content == null) {
+						callback.onFailure(new SearchException("PivotContent is null. Probably an uncaught database or searcher error "));
+					} else {
+						content.setEffectiveFilter(resultFilter);
+						searchResult.setPivotTabelData(content);
+					}
+
+					getRecentSitesUsingFilter(context, pivotTable, new AsyncCallback<SiteResult>() {
+						@Override
+						public void onFailure(Throwable caught) {
+							callback.onFailure(caught);
+						}
+
+						@Override
+						public void onSuccess(SiteResult resultSites) {
+							searchResult.setRecentAdditions(resultSites.getData());
+							callback.onSuccess(searchResult);
+						}
+					});
+				}
+			}
+		});
 	}
 
 	private PivotTableReportElement createSearchPivotTableElement() {
@@ -71,17 +88,16 @@ public class SearchHandler implements CommandHandlerAsync<Search, SearchResult> 
 	}
 
 	private void getRecentSitesUsingFilter(ExecutionContext context,
-			final AsyncCallback<SearchResult> callback,
-			final PivotTableReportElement pivotTable, final SearchResult result) {
+			final PivotTableReportElement pivotTable,
+			final AsyncCallback<SiteResult> callback) {
 		
-		GetSitesHandler getSitesHandler = new GetSitesHandler(db);
 		GetSites getSites = new GetSites();
 		getSites.setSortInfo(new SortInfo("DateEdited", SortDir.DESC));
 		getSites.setLimit(10);
 		//Filter siteFilter = pivotTable.getFilter()
 		getSites.setFilter(pivotTable.getFilter());
 		
-		getSitesHandler.execute(getSites, context, new AsyncCallback<SiteResult>() {
+		context.execute(getSites, new AsyncCallback<SiteResult>() {
 			@Override
 			public void onFailure(Throwable caught) {
 				callback.onFailure(caught);
@@ -89,7 +105,7 @@ public class SearchHandler implements CommandHandlerAsync<Search, SearchResult> 
 
 			@Override
 			public void onSuccess(SiteResult siteResult) {
-				result.setRecentAdditions(siteResult.getData());
+				callback.onSuccess(siteResult);
 			}
 		});
 	}
@@ -107,13 +123,13 @@ public class SearchHandler implements CommandHandlerAsync<Search, SearchResult> 
 		return content;
 	}
 	
-	private void createFilterFromQuery(String query, final PivotTableReportElement pivotTable, final AsyncCallback<SearchResult> callback) {
-		AllSearcher allSearcher = new AllSearcher(db);
+	private void createFilterFromQuery(String query, final PivotTableReportElement pivotTable, SqlTransaction tx, final AsyncCallback<Filter> callback) {
+		AllSearcher allSearcher = new AllSearcher(tx);
 		allSearcher.searchAll(query, new AsyncCallback<Filter>() {
 			
 			@Override
 			public void onSuccess(Filter result) {
-				pivotTable.setFilter(result);
+				callback.onSuccess(result);
 			}
 			
 			@Override
