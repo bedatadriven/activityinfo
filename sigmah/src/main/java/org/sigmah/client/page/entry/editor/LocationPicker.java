4e5ecc0c73e80a134b798261e0ffcc162524469a
ruud <ruud@bedatadriven.com>
2011-09-27 15:20:52 +0200

    give setters to basemodeldata deriver

--- sigmah/src/main/java/org/sigmah/client/page/entry/editor/LocationPicker.java
+++ sigmah/src/main/java/org/sigmah/client/page/entry/editor/LocationPicker.java
@@ -415,29 +415,56 @@ public class LocationPicker
 		public String getName() {
 			return (String)get("name");
 		}
+		public void setName(String name) {
+			set("name", name);
+		}
 		public String getAxe() {
 			return (String)get("axe");
+		}
+		public void setAxe(String axe) {
+			set("axe", axe);
 		}
 		public Double getLatitude() {
 			return (Double)get("latitude");
 		}
+		public void setLatitude(double latitude) {
+			set("latitude", latitude);
+		}
 		public Double getLongitude() {
 			return (Double)get("longitude");
+		}
+		public void setLongitude(double longitude) {
+			set("longitude", longitude);
 		}
 		public int getId() {
 			return (Integer)get("id");
 		}
+		public void setId(int id) {
+			set("id", id);
+		}
 		public String locationType() {
 			return (String) get("locationType");
+		}
+		public void setLocationType(String locationType) {
+			set("locationType", locationType);
 		}
 		public boolean hasCoordinates() {
 			return (Boolean)get("hasCoordinates");
 		}
+		public void setHasCoordinates(boolean hasCoordinates) {
+			set("hasCoordinates", hasCoordinates);
+		}
 		public boolean hasAxe() {
 			return (Boolean) get("hasAxe");
 		}
+		public void setHasAxe(boolean hasAxe) {
+			set("hasAxe", hasAxe);
+		}
 		public String marker() {
 			return (String) get("marker");
+		}
+		public void setMarker(String marker) {
+			set("marker", marker);
 		}
 	}
 	
