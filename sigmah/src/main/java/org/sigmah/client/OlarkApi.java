911ab803836a29f899019953e233a7ef337e62ff
alex@bedatadriven.com <alex@bedatadriven.com>
2011-10-12 09:26:46 +0200

    Fixed Olarki JSNI

--- sigmah/src/main/java/org/sigmah/client/OlarkApi.java
+++ sigmah/src/main/java/org/sigmah/client/OlarkApi.java
@@ -3,11 +3,11 @@ package org.sigmah.client;
 public class OlarkApi {
 
 	public static native void updateEmailAddress(String email) /*-{
-		olark('api.visitor.updateEmailAddress', {emailAddress: email});
+		$wnd.olark('api.visitor.updateEmailAddress', {emailAddress: email});
 	}-*/;
 	
 	public static native void updateFullName(String name) /*-{
-		olark('api.visitor.updateFullName', {fullName: name});
+		$wnd.olark('api.visitor.updateFullName', {fullName: name});
 	}-*/;
 	
 	
