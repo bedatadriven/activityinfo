9a8657eaebce23b3e8f92b1756af53875da69eff
Alex Bertram <alex@bedatadriven.com>
2011-12-05 11:44:24 +0100

    Committing missing files from previous commit

---
+++ sigmah/src/test/java/org/sigmah/server/util/date/DateUtilCalendarImplTest.java
@@ -0,0 +1,42 @@
+package org.sigmah.server.util.date;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+import org.sigmah.shared.report.model.DateRange;
+import org.sigmah.shared.report.model.DateUnit;
+
+public class DateUtilCalendarImplTest {
+
+	private DateUtilCalendarImpl util = new DateUtilCalendarImpl();
+	
+	@Test
+	public void floorMonth() {
+		Calendar fifth = Calendar.getInstance();
+		fifth.set(2011, Calendar.MAY, 5);
+		
+		Date date = util.floor(fifth.getTime(), DateUnit.MONTH);
+		
+		Calendar floored = Calendar.getInstance();
+		floored.setTime(date);
+		
+		assertThat("year", floored.get(Calendar.YEAR), equalTo(2011));
+		assertThat("month", floored.get(Calendar.MONTH), equalTo(Calendar.MAY));
+		assertThat("day", floored.get(Calendar.DATE), equalTo(1));
+	}
+	
+	@Test
+	public void lastCompleteMonth() {
+		Calendar fifth = Calendar.getInstance();
+		fifth.set(2011, Calendar.MAY, 5);
+		
+		DateRange range = util.lastCompleteMonthRange(fifth.getTime());
+		assertThat(range.getMinDate().getDate(), equalTo(1));
+		assertThat(range.getMaxDate().getDate(), equalTo(30));
+	}
+	
+}
