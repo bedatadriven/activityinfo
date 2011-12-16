4c049a7c808ddc1a423ea310d51a137e21cc8f71
mabidshafiq <mabidshafiq@gmail.com>
2011-12-16 11:01:29 +0500

    data collection form - css settings updated

--- sigmah/src/main/java/org/sigmah/client/page/entry/form/PrintDataEntryForm.java
+++ sigmah/src/main/java/org/sigmah/client/page/entry/form/PrintDataEntryForm.java
@@ -50,7 +50,7 @@ public class PrintDataEntryForm {
 		for (IndicatorGroup group : activity.groupIndicators()) {
 
 			if (group.getName() != null) {
-				builder.append("<h2> " + group.getName() + "</h2>");
+				builder.append("<h3> " + group.getName() + "</h3>");
 			}
 			builder.append("<table border=\"1px\" align=\"left\" cellpadding=\"0\" cellspacing=\"0\" class=\"form-detail\">");
 			builder.append("<tr>");
