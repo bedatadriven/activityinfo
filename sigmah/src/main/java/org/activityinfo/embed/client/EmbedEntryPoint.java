a6bc596d465520db4a141e64270bcde1edd47357
mabidshafiq <mabidshafiq@gmail.com>
2011-12-16 19:31:58 +0500

    Embed, partnerId filters added.

--- sigmah/src/main/java/org/activityinfo/embed/client/EmbedEntryPoint.java
+++ sigmah/src/main/java/org/activityinfo/embed/client/EmbedEntryPoint.java
@@ -1,23 +1,22 @@
 package org.activityinfo.embed.client;
 
+import java.util.List;
+import java.util.Map;
+import org.sigmah.client.i18n.I18N;
 import org.sigmah.client.page.entry.SiteGridPanel;
 import org.sigmah.client.page.entry.grouping.NullGroupingModel;
 import org.sigmah.shared.command.Filter;
-import org.sigmah.shared.dto.AnonymousUser;
 import org.sigmah.shared.report.model.DimensionType;
-
 import com.allen_sauer.gwt.log.client.Log;
 import com.extjs.gxt.ui.client.GXT;
 import com.extjs.gxt.ui.client.util.Theme;
-import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.Html;
 import com.extjs.gxt.ui.client.widget.Viewport;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
-import com.extjs.gxt.ui.client.widget.layout.LayoutData;
+import com.google.common.collect.Lists;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window.Location;
-import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -28,7 +27,7 @@ public class EmbedEntryPoint implements EntryPoint {
 	 */
 	@Override
 	public void onModuleLoad() {
-		
+
 		Log.info("Application: onModuleLoad starting");
 		Log.info("Application Permutation: " + GWT.getPermutationStrongName());
 
@@ -44,34 +43,48 @@ public class EmbedEntryPoint implements EntryPoint {
 			});
 		}
 
-		if(Location.getParameter("activityId") == null){
+		if (Location.getParameter("activityId") == null) {
 			Log.info("Could not obtain Activity Id!");
-			
-			Html html = new Html("No activity id provided.");
+
+			Html html = new Html(I18N.CONSTANTS.noActivityIdProvided());
 			addToRootPanel(html);
-	    	return;
-	    }
-		
+			return;
+		}
+
 		GXT.setDefaultTheme(Theme.BLUE, true);
 
-	    final EmbedInjector injector = GWT.create(EmbedInjector.class);
-	    	
-        int activityId = Integer.valueOf(Location.getParameter("activityId")); 
-        
+		final EmbedInjector injector = GWT.create(EmbedInjector.class);
+
+		SiteGridPanel panel = new SiteGridPanel(injector.getDispatcher());
+		panel.load(NullGroupingModel.INSTANCE, createFilter());
+
+		addToRootPanel(panel);
+	}
+
+	private Filter createFilter() {
 		Filter filter = new Filter();
+		
+		int activityId = Integer.valueOf(Location.getParameter("activityId"));
 		filter.addRestriction(DimensionType.Activity, activityId);
 		
-		SiteGridPanel panel = new SiteGridPanel(injector.getDispatcher());
-		panel.load(NullGroupingModel.INSTANCE, filter);
+		Map<String, List<String>> parameterMap = Location.getParameterMap();
+		List<String> ids = parameterMap.get("partnerId");
+		if (ids != null) {
+			List<Integer> partnerIds = Lists.newArrayList();
+			for (String id : ids) {
+				partnerIds.add(Integer.valueOf(id));
+			}
+			filter.addRestriction(DimensionType.Partner, partnerIds);
+		}
 
-		addToRootPanel(panel);              
+		return filter;
 	}
-	
-	private void addToRootPanel(Widget panel){
+
+	private void addToRootPanel(Widget panel) {
 		Viewport viewport = new Viewport();
-        viewport.setLayout(new FitLayout());
-        viewport.add(panel);
-        
-        RootPanel.get().add(viewport);
+		viewport.setLayout(new FitLayout());
+		viewport.add(panel);
+
+		RootPanel.get().add(viewport);
 	}
 }
