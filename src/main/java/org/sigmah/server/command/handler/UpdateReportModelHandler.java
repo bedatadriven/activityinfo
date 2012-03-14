fe90152b14917f6d39887a08c7d5f34c1bf6fc83
umadumdi <umadjaved33@gmail.com>
2012-03-14 16:35:30 +0500

    implemented json report serializer

--- src/main/java/org/sigmah/server/command/handler/UpdateReportModelHandler.java
+++ src/main/java/org/sigmah/server/command/handler/UpdateReportModelHandler.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.xml.bind.JAXBException;
 
+import org.sigmah.client.page.report.json.ReportJsonFactory;
 import org.sigmah.server.database.hibernate.entity.ReportDefinition;
 import org.sigmah.server.database.hibernate.entity.User;
 import org.sigmah.server.report.ReportParserJaxb;
@@ -11,33 +12,41 @@ import org.sigmah.shared.command.UpdateReportModel;
 import org.sigmah.shared.command.result.CommandResult;
 import org.sigmah.shared.exception.CommandException;
 import org.sigmah.shared.exception.UnexpectedCommandException;
+import org.sigmah.shared.report.model.Report;
 
 import com.google.inject.Inject;
 
 public class UpdateReportModelHandler implements CommandHandler<UpdateReportModel> {
 	
 	private final EntityManager em;
+	private final ReportJsonFactory reportJsonFactory;
 	
 	@Inject
-	public UpdateReportModelHandler(EntityManager em){
+	public UpdateReportModelHandler(EntityManager em, ReportJsonFactory reportJsonFactory){
 		this.em = em;
+		this.reportJsonFactory = reportJsonFactory;
 	}
 
 	@Override
 	public CommandResult execute(UpdateReportModel cmd, User user)
 			throws CommandException {
+		
+		Report report = reportJsonFactory.deserialize(cmd.getReportJsonModel());
+		
 		Query query = em
 				.createQuery(
 						"select r from ReportDefinition r where r.id in (:id)")
-				.setParameter("id", cmd.getModel().getId());
+				.setParameter("id", report.getId());
 
 		ReportDefinition result = (ReportDefinition) query.getSingleResult();
-		result.setTitle(cmd.getModel().getTitle());
-		try{
-			result.setXml(ReportParserJaxb.createXML(cmd.getModel()));
-		} catch (JAXBException e) {
-			throw new UnexpectedCommandException(e);
-		}
+		
+		result.setTitle(report.getTitle());
+		result.setJson(cmd.getReportJsonModel());
+//		try{
+//			result.setXml(ReportParserJaxb.createXML(cmd.getModel()));
+//		} catch (JAXBException e) {
+//			throw new UnexpectedCommandException(e);
+//		}
 		
 		em.persist(result);
 		
