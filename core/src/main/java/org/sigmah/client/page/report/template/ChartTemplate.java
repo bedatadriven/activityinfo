907196a607165daefb9ca91dc47dad97d4300599
Alex Bertram <alex@bedatadriven.com>
2012-05-01 15:07:33 +0200

    Added default dimension choices for charts & tables Closes Issue #134

--- core/src/main/java/org/sigmah/client/page/report/template/ChartTemplate.java
+++ core/src/main/java/org/sigmah/client/page/report/template/ChartTemplate.java
@@ -3,6 +3,8 @@ package org.sigmah.client.page.report.template;
 import org.sigmah.client.dispatch.Dispatcher;
 import org.sigmah.client.i18n.I18N;
 import org.sigmah.client.page.report.json.ReportSerializer;
+import org.sigmah.shared.report.model.DateDimension;
+import org.sigmah.shared.report.model.DateUnit;
 import org.sigmah.shared.report.model.PivotChartReportElement;
 import org.sigmah.shared.report.model.Report;
 
@@ -21,9 +23,12 @@ public class ChartTemplate extends ReportTemplate {
 	
 	@Override
 	public void create(final AsyncCallback<Integer> callback) {
-	
+
+		PivotChartReportElement chart = new PivotChartReportElement();
+		chart.setCategoryDimension(new DateDimension(DateUnit.YEAR));
+		
 		Report report = new Report();
-		report.addElement(new PivotChartReportElement());
+		report.addElement(chart);
 		
 		save(report, callback);
 	}
--- core/src/main/java/org/sigmah/client/page/report/template/PivotTableTemplate.java
+++ core/src/main/java/org/sigmah/client/page/report/template/PivotTableTemplate.java
@@ -2,7 +2,10 @@ package org.sigmah.client.page.report.template;
 
 import org.sigmah.client.dispatch.Dispatcher;
 import org.sigmah.client.i18n.I18N;
-import org.sigmah.client.page.report.json.ReportSerializer;
+import org.sigmah.shared.report.model.DateDimension;
+import org.sigmah.shared.report.model.DateUnit;
+import org.sigmah.shared.report.model.Dimension;
+import org.sigmah.shared.report.model.DimensionType;
 import org.sigmah.shared.report.model.PivotTableReportElement;
 import org.sigmah.shared.report.model.Report;
 
@@ -20,8 +23,13 @@ public class PivotTableTemplate extends ReportTemplate {
 
 	@Override
 	public void create(AsyncCallback<Integer> callback) {
+		PivotTableReportElement table = new PivotTableReportElement();
+		table.addColDimension(new DateDimension(DateUnit.YEAR));
+		table.addColDimension(new DateDimension(DateUnit.MONTH));
+		table.addRowDimension(new Dimension(DimensionType.Partner));
+		
 		Report report = new Report();
-		report.addElement(new PivotTableReportElement());
+		report.addElement(table);
 		
 		save(report, callback);
 	}
