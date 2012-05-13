486d474aaa229e94a55ce27ffe3a1e5834d16c7b
alex@bedatadriven.com <alex@bedatadriven.com>
2012-05-13 10:51:12 +0200

    Implemented hashCode() / equals() for GetPartnersWithSites
    This allows the command to be merged when requested by multiple components
    at the same time.

--- src/main/java/org/activityinfo/shared/command/GetPartnersWithSites.java
+++ src/main/java/org/activityinfo/shared/command/GetPartnersWithSites.java
@@ -2,6 +2,8 @@ package org.activityinfo.shared.command;
 
 import org.activityinfo.shared.command.result.PartnerResult;
 
+import com.google.common.base.Objects;
+
 /**
  * Retrieves a list of partners 
  * @author alex
@@ -26,5 +28,24 @@ public class GetPartnersWithSites implements Command<PartnerResult> {
 	public void setFilter(Filter filter) {
 		this.filter = filter;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((filter == null) ? 0 : filter.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GetPartnersWithSites other = (GetPartnersWithSites) obj;
+		return Objects.equal(other.filter, filter);
+	}
 }
