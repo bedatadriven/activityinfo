20fafac593511af5bba982be52e76ede56ccf407
alex@bedatadriven.com <alex@bedatadriven.com>
2012-09-28 08:49:14 +0200

    Adds missing Tables class

---
+++ src/main/java/org/activityinfo/shared/db/Tables.java
@@ -0,0 +1,36 @@
+package org.activityinfo.shared.db;
+
+public class Tables {
+
+	public static final String ACTIVITY = "activity";
+	public static final String ADMIN_ENTITY =  "adminentity";
+	public static final String ADMIN_LEVEL =  "adminlevel";
+	public static final String ATTRIBUTE =  "attribute";
+	public static final String ATTRIBUTE_GROUP =  "attributegroup";
+	public static final String ATTRIBUTE_GROUP_IN_ACTIVITY =  "attributegroupinactivity";
+	public static final String ATTRIBUTE_VALUE =  "attributevalue";
+	public static final String AUTHENTICATION =  "authentication";
+	public static final String BASEMAP =  "basemap";
+	public static final String COUNTRY =  "country";
+	public static final String INDICATOR =  "indicator";
+	public static final String INDICATOR_LINK =  "indicatorlink";
+	public static final String INDICATOR_VALUE =  "indicatorvalue";
+	public static final String LOCATION =  "location";
+	public static final String LOCATION_ADMIN_LINK =  "locationadminlink";
+	public static final String LOCATION_TYPE =  "locationtype";
+	public static final String LOCKED_PERIOD =  "lockedperiod";
+	public static final String PARTNER =  "partner";
+	public static final String PARTNER_IN_DATABASE =  "partnerindatabase";
+	public static final String PROJECT =  "project";
+	public static final String REPORTING_PERIOD =  "reportingperiod";
+	public static final String REPORT_SUBSCRIPTION =  "reportsubscription";
+	public static final String REPORT_TEMPLATE =  "reporttemplate";
+	public static final String REPORT_VISIBILITY =  "reportvisibility";
+	public static final String SITE =  "site";
+	public static final String SITE_ATTACHMENT =  "siteattachment";
+	public static final String TARGET =  "target";
+	public static final String TARGET_VALUE =  "targetvalue";
+	public static final String USER_DATABASE =  "userdatabase";
+	public static final String USER_LOGIN =  "userlogin";
+	public static final String USER_PERMISSION =  "userpermission";	
+}
