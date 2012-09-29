86954250b7f2fff0049f3784a8818f7b86c45fa1
alex@bedatadriven.com <alex@bedatadriven.com>
2012-09-29 20:16:10 +0200

    Defines caching policy in appengine-web.xml
    Removes CacheFilter.java which is no longer necessary.

--- src/main/java/org/activityinfo/server/endpoint/gwtrpc/GwtRpcModule.java
+++ src/main/java/org/activityinfo/server/endpoint/gwtrpc/GwtRpcModule.java
@@ -14,11 +14,6 @@ public class GwtRpcModule extends ServletModule {
 
     @Override
     protected void configureServlets() {
-        // The CacheFilter assures that static files marked with
-        // .nocache (e.g. strongly named js permutations) get sent with the
-        // appropriate cache header so browsers don't ask for it again.
-        filter("/ActivityInfo/*").through(CacheFilter.class);
-
         serve("/ActivityInfo/cmd").with(CommandServlet.class);
         serve("/Embed/cmd").with(CommandServlet.class);
         serve("/ActivityInfo/download").with(DownloadServlet.class);
