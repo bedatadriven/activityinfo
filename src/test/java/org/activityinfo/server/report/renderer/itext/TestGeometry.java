735f93c8b53696751e22d3ace780af456877a031
Alex Bertram <alex@bedatadriven.com>
2012-10-30 12:51:50 +0100

    Fixed Polygon test

--- src/test/java/org/activityinfo/server/report/renderer/itext/TestGeometry.java
+++ src/test/java/org/activityinfo/server/report/renderer/itext/TestGeometry.java
@@ -1,17 +1,12 @@
 package org.activityinfo.server.report.renderer.itext;
 
-import java.io.File;
-
 import org.activityinfo.server.geo.AdminGeometryProvider;
-import org.activityinfo.server.geo.LocalGeometryStorage;
+import org.activityinfo.server.geo.ClasspathGeometryProvider;
 import org.activityinfo.server.geo.WkbGeometryProvider;
 
 public abstract class TestGeometry {
 
 	public static AdminGeometryProvider get() {
-		//return new WkbGeometryProvider(new ClasspathGeometryProvider());
-		//return new WkbGeometryProvider(new RemoteGcsStorage());
-		return new WkbGeometryProvider(new LocalGeometryStorage(
-				new File("C:/Users/Alex/aigeodb/geometry")));
+		return new WkbGeometryProvider(new ClasspathGeometryProvider());
 	}
 }
