326bb394ce5fa12ebb925ca972c83ebd4495e708
Alex Bertram <alex@bedatadriven.com>
2012-12-12 22:32:01 +0100

    Test case for admin clustering
    Verify that even sites without coordinates are included in map markers

--- src/test/java/org/activityinfo/shared/command/GeneratePivotTableHandlerTest.java
+++ src/test/java/org/activityinfo/shared/command/GeneratePivotTableHandlerTest.java
@@ -7,15 +7,22 @@ import org.activityinfo.server.database.OnDataSet;
 import org.activityinfo.server.database.TestDatabaseModule;
 import org.activityinfo.server.report.ReportModule;
 import org.activityinfo.shared.exception.CommandException;
+import org.activityinfo.shared.report.content.MapContent;
+import org.activityinfo.shared.report.content.MapMarker;
 import org.activityinfo.shared.report.content.PivotContent;
 import org.activityinfo.shared.report.model.AttributeGroupDimension;
 import org.activityinfo.shared.report.model.Dimension;
 import org.activityinfo.shared.report.model.DimensionType;
+import org.activityinfo.shared.report.model.MapReportElement;
 import org.activityinfo.shared.report.model.PivotTableReportElement;
+import org.activityinfo.shared.report.model.clustering.AdministrativeLevelClustering;
+import org.activityinfo.shared.report.model.layers.BubbleMapLayer;
 import org.activityinfo.test.InjectionSupport;
 import org.activityinfo.test.Modules;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 @RunWith(InjectionSupport.class)
 @Modules({TestDatabaseModule.class, ReportModule.class})
@@ -54,6 +61,36 @@ public class GeneratePivotTableHandlerTest extends CommandTestCase2 {
 		PivotContent content = execute(new GeneratePivotTable(element));
 //		TODO real test
 //		System.out.println(content.getData());
+		
+	}
+	
+	@Test
+	public void adminMap() {
+		
+		AdministrativeLevelClustering clustering = new AdministrativeLevelClustering();
+		clustering.getAdminLevels().add(1);
+		
+		BubbleMapLayer layer = new BubbleMapLayer();
+		layer.setClustering(clustering);
+		
+		Filter filter = new Filter();
+		filter.addRestriction(DimensionType.Site, 3);
+		
+		MapReportElement map = new MapReportElement();
+		map.addLayer(layer);
+		map.setFilter(filter);
+		
+		MapContent result = (MapContent)execute(new GenerateElement(map));
+
+		System.out.println(result.getMarkers());
+
+		MapMarker marker = result.getMarkers().get(0);
+		assertThat(marker.getSiteIds().size(), equalTo(1));
+		assertThat(marker.getSiteIds().get(0), equalTo(3));
+		
+		assertThat(result.getUnmappedSites().size(), equalTo(0));
+		
+		
 		
 	}
 	
