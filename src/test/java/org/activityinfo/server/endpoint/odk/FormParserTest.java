2eed25ab7949c2c9f526afca5b3937f8e95f35f1
Jasper Fontaine <jfontaine@codegap.com>
2013-08-15 11:33:52 +0200

    [AI-359] ODK Collect Integration: fixed test

--- src/test/java/org/activityinfo/server/endpoint/odk/FormParserTest.java
+++ src/test/java/org/activityinfo/server/endpoint/odk/FormParserTest.java
@@ -2,8 +2,6 @@ package org.activityinfo.server.endpoint.odk;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 
 import org.activityinfo.server.database.TestDatabaseModule;
@@ -13,8 +11,6 @@ import org.activityinfo.test.Modules;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Files;
 import com.google.inject.Inject;
 
 @RunWith(InjectionSupport.class)
@@ -27,10 +23,30 @@ public class FormParserTest {
     @Inject
     private FormParser parser;
 
+    // @formatter:off
+    private static String xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+        "<data id=\"siteform\">" +
+            "<meta>" +
+                "<instanceID>uuid:23b56e39-ef50-4510-b85f-c454cd5465c1</instanceID>" +
+            "</meta>" +
+            "<activity>927</activity>" +
+            "<partner>274</partner>" +
+            "<locationname>Some location</locationname>" +
+            "<gps>52.144802074999994 5.377899974999999</gps>" +
+            "<date1>2012-07-05</date1>" +
+            "<date2>2013-08-07</date2>" +
+            "<I4410>1.1</I4410>" +
+            "<I4411>2.5</I4411>" +
+            "<AG991>2419 2420</AG991>" +
+            "<comments>Some comment</comments>" +
+        "</data>";
+ // @formatter:on
+
     @Test
     public void testParseSiteForm() throws Exception {
-        URL url = FormParserTest.class.getResource("/org/activityinfo/server/endpoint/odk/form-instance.xml");
-        String xml = Files.toString(new File(url.getPath()), Charsets.UTF_8);
+        // URL url = FormParserTest.class.getResource("/org/activityinfo/server/endpoint/odk/form-instance.xml");
+        // String xml = Files.toString(new File(url.getPath()), Charsets.UTF_8);
 
         SiteFormData data = parser.parse(xml);
 
@@ -42,6 +58,9 @@ public class FormParserTest {
         assertEquals("2012-07-05", new SimpleDateFormat("yyyy-MM-dd").format(data.getDate1()));
         assertEquals("2013-08-07", new SimpleDateFormat("yyyy-MM-dd").format(data.getDate2()));
         assertEquals("Some comment", data.getComments());
+
+        assertEquals((Double) 52.144802074999994D, (Double) data.getLatitude());
+        assertEquals((Double) 5.377899974999999, (Double) data.getLongitude());
 
         assertEquals(2, data.getIndicators().size());
         assertEquals(4410, data.getIndicators().get(0).getId());
